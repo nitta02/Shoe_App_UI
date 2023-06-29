6b8ebc2b64dd5f55d50870fa4a94910ff2aa418d
nittoranjansarkertanmoy02 <nittaranjansarkertanmoy2022@gmail.com>
2023-06-30 00:31:33 +0600

    day1

--- lib/screens/shoes_details.dart
+++ lib/screens/shoes_details.dart
@@ -48,41 +48,171 @@ class _ShoesDetailsState extends State<ShoesDetails> {
               height: MediaQuery.of(context).size.height,
               width: double.infinity,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(widget.image),
-                ),
-              ),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(widget.image),
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(5, 10),
+                      blurRadius: 10.0,
+                    )
+                  ]),
               child: Stack(children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        width: 35,
-                        height: 35,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
                         child: const Icon(
                           Icons.arrow_back_ios,
                           size: 30,
                           color: Colors.white,
                         ),
                       ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: const Icon(Icons.favorite_border),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Sneakers',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 30,
+                        letterSpacing: 1.8,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      'Nikee',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 20,
+                        letterSpacing: 1.8,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          margin: const EdgeInsets.only(right: 20),
+                          decoration: const BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '40',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          margin: const EdgeInsets.only(right: 20),
+                          decoration: const BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '40',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          margin: const EdgeInsets.only(right: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '40',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          margin: const EdgeInsets.only(right: 20),
+                          decoration: const BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '40',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50,
                     ),
                     Container(
-                      width: 35,
-                      height: 35,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: const Center(
+                        child: Text(
+                          'Buy Now',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1.8,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
-                      child: const Icon(Icons.favorite_border),
+                    ),
+                    const SizedBox(
+                      height: 50,
                     ),
                   ],
-                ),
+                )
               ]),
             ),
           ),
